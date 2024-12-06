1111a1d8b03fdb1eb93f07354c8d8a3dcbb44436
KananHasanov747 <kanan.hasanov747@gmail.com>
2024-12-07 03:51:32 +0400

    added 'notion-client'

---
+++ tgbot/services/notion_client.py
@@ -0,0 +1,32 @@
+from notion_client import AsyncClient
+from tgbot.database import session
+from tgbot.models import Link
+
+
+class NotionService:
+    def __init__(self, notion_token: str) -> None:
+        self.client = AsyncClient(auth=notion_token)
+
+    async def add_link(
+        self, database_id: str, url: str, title: str = "", category: str = ""
+    ):
+        properties = {"URL": {"url": url}}
+
+        if title:
+            properties["Title"] = {"title": [{"text": {"content": title}}]}
+
+        if category:
+            properties["Category"] = {"select": {"name": category}}
+
+        # Creating a new page in the database
+        response = await self.client.pages.create(
+            parent={"database_id": database_id}, properties=properties
+        )
+
+        session.add(
+            Link(url=url, title=title, category=category, database_id=database_id)
+        )
+
+        await session.commit()
+
+        return response
