79d0b09170718a792c03e765d31deddfec8bd67a
KananHasanov747 <kanan.hasanov747@gmail.com>
2024-12-08 15:57:13 +0400

    removed Workspace and Database models for convenience; changed workspace and database fields in User model

--- tgbot/models/__init__.py
+++ tgbot/models/__init__.py
@@ -14,34 +14,8 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, unique=True, nullable=False)
-    curr_workspace_id = Column(
-        Text, ForeignKey("workspaces.workspace_id", ondelete="SET NULL"), nullable=True
-    )
-    curr_database_id = Column(
-        Text, ForeignKey("databases.database_id", ondelete="SET NULL"), nullable=True
-    )
-
-
-class Workspace(Base):
-    __tablename__ = "workspaces"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    workspace_name = Column(Text, nullable=False)
-    workspace_id = Column(Text, unique=True, nullable=False)
-    telegram_user_id = Column(
-        Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False
-    )
-
-
-class Database(Base):
-    __tablename__ = "databases"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    database_name = Column(Text, nullable=False)
-    database_id = Column(Text, unique=True, nullable=False)
-    workspace_id = Column(
-        Text, ForeignKey("workspaces.workspace_id", ondelete="CASCADE"), nullable=False
-    )
+    workspace_id = Column(Text, nullable=False)
+    database_id = Column(Text, nullable=False)
 
 
 class Link(Base):
@@ -52,5 +26,5 @@ class Link(Base):
     title = Column(Text)
     category = Column(Text)
     database_id = Column(
-        Text, ForeignKey("databases.database_id", ondelete="CASCADE"), nullable=False
+        Text, ForeignKey("users.database_id", ondelete="CASCADE"), nullable=False
     )
